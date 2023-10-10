0a092c1f6e2e9799e21346d3889d6faefc6a48ba
Maike2961 <mayki.silva@hotmail.com>
2023-10-10 15:34:19 -0300

    Criação de Site com Python

---
+++ meu_app.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(page_title="Meu Site Streamlit")
+
+
+hide_st = """
+<style>
+#MainMenu{visibility: hidden;}
+.st-emotion-cache-ch5dnh {visibility: hidden;}
+footer{visibility: hidden;}
+</style>
+"""
+st.markdown(hide_st, unsafe_allow_html=True)
+
+
+
+with st.container():
+    st.title("Registro de Contratos")
+    st.subheader("Meu primeiro site com o Streamlit")
+    st.write("Informações sobre os contratos fechados pela Hash&Co ao longo de maio")
+    st.write("Quer aprender Python? [Clique aqui](https://lp.hashtagtreinamentos.com/)")
+    
+@st.cache_data #armazena no cache do navegator do usuario as informações que estao sendo carregados
+def carregar_dados():
+    tabela =  pd.read_csv("resultados.csv")
+    return tabela
+    
+with st.container():
+    st.write("---")
+    qtdes_dias = st.selectbox("Selecione o período", ["7D", "15D", "21D","30D"])
+    num_dias = int(qtdes_dias.replace("D", ""))
+    dados = carregar_dados()
+    dados = dados[-num_dias:] #contando o array de atras para frente
+    
+    st.area_chart(dados, x="Data", y="Contratos")
